8c640cc0ec85a40778c4829947e1b9dedf610525
Daniel Ferreira <silva_120000@yahoo.com.br>
2026-05-02 05:35:30 -0300

    Create update and deletion routes.

--- app.py
+++ app.py
@@ -57,6 +57,32 @@ def read_user(id_user):
     return jsonify({"username": user.username})
   return jsonify({"message": "Usuário não encontrado."}), 404
 
+@app.route('/user/<int:id_user>', methods=["PUT"])
+@login_required
+def update_user(id_user):
+  data = request.json
+  user = User.query.get(id_user)
+  if user and data.get("password"):
+    user.password = data.get("password")
+    db.session.commit()
+    return jsonify({"message": f"Usuário {id_user} atualizado com sucesso"})
+  
+  return jsonify({"message": "Usuario não encontrado"}), 404
+
+@app.route('/user/<int:id_user>', methods=["DELETE"])
+@login_required
+def delete_user(id_user):
+  user = User.query.get(id_user)
+  
+  if id_user == current_user.id:
+    return jsonify({"message": "Deleção não permitida."})
+
+  if user:
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": f"Usuário {id_user} deletado com sucesso."})
+  return jsonify({"message": "Usuário não encontrado."}), 404
+
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
   return "Hello world"
